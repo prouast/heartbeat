ed38feb16a19ea9f66e0c9b54ad9924bca83d158
Philipp Rouast <prouast@gmail.com>
2016-02-29 15:02:08 +1100

    Initial Commit

---
+++ Heartbeat/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Heartbeat
+//
+//  Created by Philipp Rouast on 29/02/2016.
+//  Copyright © 2016 Philipp Roüast. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
